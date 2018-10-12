e95cf63b73df3d24c816cfe3e9a44e3aafbf6eed
nanophate <nanophate@me.com>
2018-10-12 21:54:27 +0900

    Fix: Design

--- sechack365/week1/request.rb
+++ sechack365/week1/request.rb
@@ -13,3 +13,36 @@ request["ocp-apim-subscription-key"] = ENV['AZURE_SUBSCRIPTION_TEXT_ANALYSIS']
 request.body = File.read(formatter.json)
 response = http.request(request)
 puts response.read_body
+
+
+二軸ではんだんできる
+文脈が否定て　フレーズではわからん
+
+すれ違いの
+選ぶ語彙の個人差
+
+
+
+
+文脈が
+個人差がアゥタリ比較があると
+
+相手は誤解アラート
+
+言葉のつk伊方
+
+個人分析した上で
+  - 違う文脈アラート
+
+
+ポップぱ
+
+
+差の大きさ
+ぶんみゃくを　みないと
+
+
+絵文字に
+
+
+line 
