9c2aa41a1466df5eef2ab544730919efaca2575d
nanophate <nanophate@me.com>
2018-10-12 13:32:23 +0900

    Add: output for date  and sentiment

--- sechack365/week1/sentiment_result_modify.rb
+++ sechack365/week1/sentiment_result_modify.rb
@@ -1,11 +1,18 @@
 require 'json'
 require 'pry'
 File.open("sentiment_result.json") do |file|
-  binding.pry
   json = JSON.load(file)
 
-  json["documents"].each do |f|
-    binding.pry
+  datetime = []
+  json["documents"].each do |e|
+    datetime.push(e["datetime"])
+  end
 
+  uniq_date = datetime.uniq
+
+  uniq_date.each do |date|
+    on_date_element = json["documents"].select {|element| element["datetime"] == date }
+    on_date_score = on_date_element.map{ |e| e["score"].to_f }.sum / on_date_element.count
+    puts "#{date}, #{on_date_score}" # もう時間がないのでコピペで excel へwwww😭
   end
 end
