7350c249557ea0f8da9c85188dfe3fef9b3c3230
nanophate <nanophate@me.com>
2018-10-12 23:43:35 +0900

    Improve design and visiblity

--- sechack365/week2/app.rb
+++ sechack365/week2/app.rb
@@ -27,15 +27,26 @@ post '/user' do
   uri = "https://twitter.com/@#{params['name']}"
   doc = Nokogiri::HTML(open(uri),nil,"utf-8")
 
-  @nano = []
+  @users_tweet = []
 
-  doc.css(".js-tweet-text-container").each do |tweet_text|
-    tweet_raw = tweet_text.text.gsub("\n","").strip
+  doc.css(".content").each do |tweet_text|
+    tweet_raw = tweet_text.css('.js-tweet-text-container').text.gsub("\n","").strip
     tweet = tweet_raw.gsub(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, "")
+    username = tweet_text.css('.FullNameGroup').text.gsub("\n","").strip
+
     request.body = "{'documents':[{'language':'ja','id':'1','text': '#{tweet}'}]}".gsub("\'","\"")
     response = http.request(request)
-    nanote = JSON.load(response.body)
-    @nano.push("#{tweet} , #{nanote["documents"].first["score"]}")
+    json_body = JSON.load(response.body)
+
+    tweet_attributes = Hash.new { |hash, key| key }
+
+    next if tweet.empty?
+
+    tweet_attributes[:tweet_text] = tweet
+    tweet_attributes[:score] = json_body["documents"].first["score"]
+    tweet_attributes[:user] = username
+
+    @users_tweet.push(tweet_attributes)
   end
 
   haml :user
